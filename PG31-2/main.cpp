69af5002cd62c59f91ddebb8528c8fd408a68a7a
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2023-11-01 10:39:46 +0900

    PG

--- PG31-2/main.cpp
+++ PG31-2/main.cpp
@@ -18,45 +18,26 @@ int main()
 	std::function<void(int*)> fx = [](int* s) {printf("%d•b‘Ò‚Â\n", *s); };
 	srand((unsigned int)time(NULL));
 
-	int n = 0;
-	int m = 3;
+	int evennumber = 0;
+	int oddnumber = 3;
 	int dice;
 
 	dice = rand() % 6 + 1;
 	printf("0‚©1‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
-	scanf_s("&d", &n);
+	scanf_s("&d", &evennumber);
 
-	SetTimeout(fx, m);
+	SetTimeout(fx, oddnumber);
 
-	if (n == 0)
+	if (evennumber == dice % 2)
 	{
-		if (dice % 2 == 0)
-		{
-			
+		printf("³‰ğ\n");
 
-			printf("³‰ğ\n");
-
-		}
-		else
-		{
-			
-			printf("•s³‰ğ\n");
-
-		}
 	}
 	else
 	{
-		if (dice % 2 == 1)
-		{
-			
-			printf("³‰ğ\n");
-		}
-		else
-		{
-			
-			printf("•s³‰ğ\n");
 
-		}
+		printf("•s³‰ğ\n");
+
 	}
 
 	printf("³‰ğ‚Í %d ‚Å‚µ‚½", dice);
