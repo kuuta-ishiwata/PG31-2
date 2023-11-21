0d9973f35cf374fb82c3292f637b6c64ab035263
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2023-11-21 10:38:39 +0900

    PG

--- PG31-2/main.cpp
+++ PG31-2/main.cpp
@@ -1,19 +1,11 @@
 #include <stdio.h>
-#include "Enemy.h"
 
 int main()
 {
-    Enemy enemy;
+    
 
-    bool GameLoop = true;
 
-    int i = 0;
 
-    while (i < 3)
-    {
-        enemy.Update();
-        i++;
-    }
 }
 
 
