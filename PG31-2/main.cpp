86713f510d746c1915e19c0ad60703a96ab05dc5
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2023-11-21 11:42:37 +0900

    PG

--- PG31-2/main.cpp
+++ PG31-2/main.cpp
@@ -42,7 +42,6 @@ int main()
 	printf("int,int : %d\n", B1.Min(1, 15));
 	printf("float,float : %f\n", B2.Min(4.0, 12.0));
 	printf("double,double : %lf\n", B3.Min(2.0, 6.0));
-
 	printf("int, float : %d\n", B4.Min(5, 13.0));
 	printf("float, double : %f\n", B5.Min(23.0, 43.0));
 	printf("double, int : %lf\n", B6.Min(56.0, 33));
